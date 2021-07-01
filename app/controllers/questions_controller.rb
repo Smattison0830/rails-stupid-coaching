d25eb45e4a14e818bb6835f99d52df2a4e5af98c
Spencer Mattison <spenceramattison@gmail.com>
2021-07-01 09:11:52 +0900

    updated css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,14 +6,14 @@ class QuestionsController < ApplicationController
 
   def answer
   @user_answer = params[:question]
-  @stupid_answer = ""
+  @coach_answer = ""
 
     if @user_answer.downcase == "I am going to work right now!".downcase
-      return ""
+      return "Great!"
     elsif @user_answer.include? "?"
-      return @stupid_answer = "Silly question, get dressed and go to work!"
+      return @coach_answer = "Silly question, get dressed and go to work!"
     else
-      return @stupid_answer = "I don't care, get dressed and go to work!"
+      return @coach_answer = "I don't care, get dressed and go to work!"
     end
   end
 end
